9bd22235dd1202ea29b541181f77cafa17b8a68b
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-11-11 17:02:29 +0300

    Update Mylist class

--- MyList.rb
+++ MyList.rb
@@ -1,5 +1,4 @@
 require_relative 'enumerable'
-
 class MyList
     include Enumerable
     def initialize(*list)
@@ -10,3 +9,15 @@ class MyList
       @list.each { |value| yield value if block_given? }
     end
   end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+# Test #all?
+p list.all? {|e| e < 5} #=>true
+p list.all? {|e| e > 5} #=>false
+# Test #any?
+p list.any? {|e| e == 2} #=> true
+p list.any? {|e| e == 5} #=> false
+# Test #filter
+p list.filter {|e| e.even?} #=> [2, 4]
+p list.filter {|e| e.odd?} #=> [1, 3]
